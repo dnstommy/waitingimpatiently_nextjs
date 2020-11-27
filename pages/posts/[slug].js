01c15963f60dac69f8459419d1c1c9a9afe7e3fd
Chris Auer <chris.auer@gmail.com>
2020-11-26 21:48:48 -0500

    changes for netlify

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -69,6 +69,6 @@ export async function getStaticPaths() {
   const allPosts = await getAllPostsWithSlug()
   return {
     paths: allPosts?.map(({ slug }) => `/posts/${slug}`) ?? [],
-    fallback: true,
+    fallback: false,
   }
 }
